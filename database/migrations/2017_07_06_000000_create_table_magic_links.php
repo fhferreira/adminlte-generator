5de5b4ed8594ed1105d529ea6a046066579bd45f
Flavio Ferreira <flaviometalvale@gmail.com>
2019-03-08 06:08:42 -0300

    new features to boilerplate

---
+++ database/migrations/2017_07_06_000000_create_table_magic_links.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableMagicLinks extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create(config('magiclink.magiclink_table', 'magic_links'), function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('token', 100);
+            $table->string('redirect_url')->nullable();
+            $table->timestamps();
+            $table->timestamp('available_at')->nullable();
+        });
+
+        try {
+            Schema::table(config('magiclink.magiclink_table', 'magic_links'), function (Blueprint $table) {
+                $table->foreign('user_id')->references(config('magiclink.user_primarykey',
+                    'id'))->on(config('magiclink.user_table', 'users'))->onDelete('cascade');
+            });
+        } catch (\Exception $e) {
+            \Log::error($e);
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists(config('magiclink.magiclink_table', 'magic_links'));
+    }
+}
